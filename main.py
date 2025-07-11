7a9c241ff21fa6b46ba971f7ee65c91c54da43d0
mamutpeter <nikitamailbox2018@gmail.com>
2025-07-11 14:18:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -166,16 +166,4 @@ async def main():
 # === Кінець main.py ===
 if __name__ == "__main__":
     import asyncio
-
-    try:
-        loop = asyncio.get_running_loop()
-        # Якщо вже є loop (Render) — створюємо task
-        loop.create_task(main())
-        import time
-        while True:
-            time.sleep(3600)
-    except RuntimeError:
-        # Якщо loop немає — створюємо й запускаємо як зазвичай
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(main())
+    asyncio.run(main())
